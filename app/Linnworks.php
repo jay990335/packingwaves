3c4e0f4bbe1f1d331fa6acdffb8014ccb30c9356
parth-onfuro <parth.onfuro@gmail.com>
2021-06-28 14:21:55 +0100

    no message

--- app/Linnworks.php
+++ app/Linnworks.php
@@ -2,10 +2,25 @@
 
 namespace App;
 
+use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Support\Facades\Storage;
+use Laravel\Passport\HasApiTokens;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Linnworks extends Model
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'token', 'applicationId', 'applicationSecret', 'created_by', 'updated_by',
+    ];
 }
